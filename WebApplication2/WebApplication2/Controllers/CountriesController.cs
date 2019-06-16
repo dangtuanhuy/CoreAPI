703ff85c023aa77c8fd0d99e531ca2de66bbc319
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2019-06-16 23:21:08 +0700

    save

--- WebApplication2/WebApplication2/Controllers/CountriesController.cs
+++ WebApplication2/WebApplication2/Controllers/CountriesController.cs
@@ -42,21 +42,48 @@ namespace WebApplication2.Controllers
         }
         //api/countries/countryId
         [HttpGet("{countryId}")]
+        [ProducesResponseType(400)]
         [ProducesResponseType(404)]
         [ProducesResponseType(200, Type = typeof(IEnumerable<CountryDto>))]
         public IActionResult GetCountry(int countryId)
         {
             if (!_countryRepository.CountryExists(countryId))
                 return NotFound();
+
             var country = _countryRepository.GetCountry(countryId);
+
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
+
             var countryDto = new CountryDto()
             {
                 Id = country.Id,
                 Name = country.Name
-        };
+            };
             return Ok(countryDto);
+        }
+        //api/countries/countryId/authors
+        [HttpGet("authors/{authorId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<CountryDto>))]
+        public IActionResult GetCountryOfAnAuthor(int authorId)
+        {
+            //if (!_authorRepository.AuthorExists(authorId))
+            //    return NotFound();
+
+            var country = _countryRepository.GetCountryOfAnAuthor(authorId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var countryDto = new CountryDto()
+            {
+                Id = country.Id,
+                Name = country.Name
+            };
+
+            return Ok(countryDto);
+        }
     }
-}
 }
